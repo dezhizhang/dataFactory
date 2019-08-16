bcb1d945e6a73dc2b8f9eacc72d2a84abcaa7fa7
dezhizhang <1541609448@qq.com>
2019-08-16 12:43:38 +0800

    更新文件

--- config/plugin.js
+++ config/plugin.js
@@ -18,3 +18,13 @@ exports.ejs = {
   enable: true,
   package: 'egg-view-ejs',
 };
+
+exports.passport = {
+  enable: true,
+  package: 'egg-passport',
+}
+
+exports.passportGithub = {
+  enable: true,
+  package: 'egg-passport-github',
+}
